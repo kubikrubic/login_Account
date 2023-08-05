1718ba96c4b60acb3bc020f1b3f8a79f441cd2ba
kubikrubic <131974022+kubikrubic@users.noreply.github.com>
2023-08-05 23:16:27 +0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var passwordInput = document.getElementById('passwordInput');
-
-passwordInput.addEventListener('click', () => {
-	if (passwordInput.type === 'password') {
-   passwordInput.type = 'text';
-   } else {
-   passwordInput.type = 'password';
-   }
-});
